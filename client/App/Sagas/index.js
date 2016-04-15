ccf142a5ee766fed21c56e31b4d563e183bfad83
Steve Kellock <steve@kellock.ca>
2016-04-15 17:56:36 -0400

    Adds an example of logging with the API.

--- client/App/Sagas/index.js
+++ client/App/Sagas/index.js
@@ -1,10 +1,17 @@
 import ApiSauce from 'apisauce'
-
 import { watchStartup } from './StartupSaga'
 import WeatherSaga from './WeatherSaga'
+import repl from '../../client'
 
 const api = ApiSauce.create({
   baseURL: 'http://openweathermap.org/data/2.1'
+})
+api.addMonitor((response) => {
+  if (response.config) {
+    repl.log(`[API] - ${response.ok} - ${response.status}`)
+  } else {
+    repl.log(`[API] - ${response.problem}`)
+  }
 })
 
 const {watchWeatherRequest} = WeatherSaga(api)
