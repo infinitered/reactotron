5a0a777706d2ce48380fec51920ec21d4a76ecea
Steve Kellock <steve@kellock.ca>
2016-04-17 13:29:10 -0400

    Cleans up the action logging.

--- server/lib/commands/redux/reduxActionDone.js
+++ server/lib/commands/redux/reduxActionDone.js
@@ -6,10 +6,10 @@ const COMMAND = 'redux.action.done'
 const process = (context, action) => {
   const {type, ms} = action.message
   const time = context.timeStamp()
-  context.reduxLogRaw(`{white-fg}${time}{/} - {yellow-fg}${type}{/}{|}{white-fg}${ms}ms{/}`)
+  context.ui.reduxBox.log(`{white-fg}${time}{/} - {blue-fg}action{/} ${type}{|}{white-fg}${ms}ms{/}`)
   if (context.reduxActionLoggingStyle === 'full') {
-    context.reduxLogRaw(action.message.action)
-    context.reduxLogRaw('')
+    context.ui.reduxBox.log(action.message.action)
+    context.ui.reduxBox.log('')
   }
 }
 
