0240ff9ddc3c8f5e8f2ed17dcf5a9cc8a411e90e
Steve Kellock <steve@kellock.ca>
2016-04-20 01:04:07 -0400

    Adds a console app for reactotron server.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,10 @@ const gulp = require('gulp')
 const babel = require('gulp-babel')
 const rollup = require('gulp-rollup')
 
-gulp.task('build', ['build-server', 'build-client'])
+gulp.task('build', ['build-server'])
 
 gulp.task('build-server', () => {
-  return gulp.src('src/server.js')
-    .pipe(rollup({}))
-    .pipe(babel({
-      presets: ['es2015', 'stage-0']
-    }))
-    .pipe(gulp.dest('./dist'))
-})
-
-gulp.task('build-client', () => {
-  return gulp.src('example/reactotron/client.js')
+  return gulp.src('server/index.js')
     .pipe(rollup({}))
     .pipe(babel({
       presets: ['es2015', 'stage-0']
