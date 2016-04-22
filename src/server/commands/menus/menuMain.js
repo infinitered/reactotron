a8f90e7e294de9f77cb320683bebae19af38aa82
Gant <GantMan@gmail.com>
2016-04-22 08:46:14 -0500

    new menu structure - clean

--- src/server/commands/menus/menuMain.js
+++ src/server/commands/menus/menuMain.js
@@ -6,8 +6,9 @@ const process = (context, action) => {
     commands: [
       {key: 'r', name: 'redux', commands: [{type: 'menu.redux'}]},
       {key: 'c', name: 'clear', commands: []},
-      {key: '-', name: 'score', commands: []},
-      {key: '.', name: 'repeat', commands: []},
+      // {key: '-', name: 'score', commands: []},
+      // {key: '.', name: 'repeat', commands: []},
+      {key: 'h', name: 'help', commands: [{type: 'menu.help'}]},
       // {key: 'd', name: 'dev menu', commands: [{type: 'menu.devMenu'}]},
       {key: 'q', name: 'quit', commands: [{type: 'program.die'}]}
     ]
