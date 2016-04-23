5ab384b84f8291e9893ac6699de086e40bcb86ba
Steve Kellock <steve@kellock.ca>
2016-04-23 18:50:04 -0400

    Integration of the name into the example.  Plus a bonus cp script!

--- examples/ReactNativeExample/index.ios.js
+++ examples/ReactNativeExample/index.ios.js
@@ -2,7 +2,7 @@ import React, { AppRegistry, Component } from 'react-native'
 import ReduxContainer from './App/Containers/ReduxContainer'
 import Reactotron from './client' // in a real app, you would use 'reactotron'
 
-Reactotron.connect({enabled: true})
+Reactotron.connect({enabled: true, name: 'Hello'})
 
 class ReactNativeExample extends Component {
   render () {
