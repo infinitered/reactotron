2bbc214610b92fcfb027ffd8e51a203c5bc31248
Steve Kellock <steve@kellock.ca>
2016-04-23 08:42:16 -0400

    Adds Android support to the example app.

--- examples/ReactNativeExample/index.android.js
+++ examples/ReactNativeExample/index.android.js
@@ -1,51 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+import React, { AppRegistry, Component } from 'react-native'
+import ReduxContainer from './App/Containers/ReduxContainer'
+import Reactotron from './client' // in a real app, you would use 'reactotron'
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+Reactotron.connect({enabled: true})
+
 
 class ReactNativeExample extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+  render () {
+    return <ReduxContainer />
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ReactNativeExample', () => ReactNativeExample);
+AppRegistry.registerComponent(
+  'ReactNativeExample',
+  () => ReactNativeExample
+)
