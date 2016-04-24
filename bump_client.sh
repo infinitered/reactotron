5f6a1857739730fb583b932a8d63a41178f86e45
Steve Kellock <steve@kellock.ca>
2016-04-24 10:19:27 -0400

    Switch to the same directory as the executing script.

--- bump_client.sh
+++ bump_client.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+
+# http://stackoverflow.com/a/246128/312785
+path="${BASH_SOURCE[0]}"
+while [ -h "$path" ] ; do
+  dir="$(cd -P "$(dirname "$path")" && pwd)"
+  path="$(readlink "$path")"
+  [[ $path == /* ]] || path="$dir/$path"
+done
+dir="$(cd -P "$(dirname "$path")" && pwd)"
+
+
 # replaces the version token and writes it back over itself
 FILE=dist/client.js
 sed -e 's/@@REACTOTRON_VERSION@@/'"$VERSION"'/g' $FILE > $FILE.tmp && mv $FILE.tmp $FILE
