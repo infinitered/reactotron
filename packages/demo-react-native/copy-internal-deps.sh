57ca750e71dca6d529859cab7011508d34eeef5f
Steve Kellock <steve@kellock.ca>
2016-07-30 15:31:55 -0400

    Use the *real* source for demo-react-native's internal deps.
    This is nicer for devs.  But don't get wrong.  It still sucks having to do this.

--- packages/demo-react-native/copy-internal-deps.sh
+++ packages/demo-react-native/copy-internal-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../reactotron-react-native/dist/index.js node_modules/reactotron-react-native/index.js
-cp ../reactotron-core-client/dist/index.js node_modules/reactotron-core-client/index.js
+cp ../reactotron-react-native/src/*.js node_modules/reactotron-react-native/
+cp ../reactotron-core-client/src/*.js node_modules/reactotron-core-client/
