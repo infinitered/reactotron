1fd9a9b9dc048ca9f8aa0089ee67cdb9a89df658
Steve Kellock <steve@kellock.ca>
2016-08-04 09:42:18 -0400

    Fixes the decorator functionality I just removed. (#88)

--- packages/demo-react-native/App/Config/ReactotronConfig.js
+++ packages/demo-react-native/App/Config/ReactotronConfig.js
@@ -1,11 +1,15 @@
 import Reactotron from 'reactotron-react-native'
 import tronsauce from 'reactotron-apisauce'
 
-Reactotron
-  .configure({
-    name: 'Demo Time',
-    onConnect: () => console.log('connected'),
-    onDisconnect: () => console.log('disconnected')
-  })
-  .use(tronsauce())
-  .connect()
+if (__DEV__) {
+  Reactotron
+    .configure({
+      name: 'Demo Time',
+      onConnect: () => console.log('connected'),
+      onDisconnect: () => console.log('disconnected')
+    })
+    .use(tronsauce())
+    .connect()
+
+  console.tron = Reactotron
+}
