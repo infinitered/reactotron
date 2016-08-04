bc81bedcb10f5697aff0e5da01c3e735191e0567
Steve Kellock <steve@kellock.ca>
2016-08-04 00:50:31 -0400

    Adds an apisauce plugin.

---
+++ packages/reactotron-apisauce/src/index.js
@@ -0,0 +1,26 @@
+import RS from 'ramdasauce'
+
+/**
+ * Sends an apisauce response to the server.
+ */
+export default config => {
+  return {
+    features: {
+      apisauce: (source) => {
+        // apisauce uses axios, so let's deconstruct that format
+        const url = RS.dotPath('config.url', source)
+        const method = RS.dotPath('config.method', source)
+        const requestData = RS.dotPath('config.data', source)
+        const requestHeaders = RS.dotPath('config.headers', source)
+        const duration = RS.dotPath('duration', source)
+        const status = RS.dotPath('status', source)
+        const body = RS.dotPath('data', source)
+        const responseHeaders = RS.dotPath('headers', source)
+        const request = { url, method, data: requestData, headers: requestHeaders }
+        const response = { body, status, headers: responseHeaders }
+
+        config.ref.apiResponse(request, response, duration)
+      }
+    }
+  }
+}
