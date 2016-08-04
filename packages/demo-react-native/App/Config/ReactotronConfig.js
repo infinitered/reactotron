2083fe644ba7136cad04e5367e1d37ca6a1f5573
Steve Kellock <steve@kellock.ca>
2016-08-04 09:31:39 -0400

    Removes the useless decorate plugin.

--- packages/demo-react-native/App/Config/ReactotronConfig.js
+++ packages/demo-react-native/App/Config/ReactotronConfig.js
@@ -1,5 +1,4 @@
 import Reactotron from 'reactotron-react-native'
-import decorate from 'reactotron-core-client/plugins/decorator'
 import tronsauce from 'reactotron-apisauce'
 
 Reactotron
@@ -8,6 +7,5 @@ Reactotron
     onConnect: () => console.log('connected'),
     onDisconnect: () => console.log('disconnected')
   })
-  .use(decorate(console, 'tron'))
   .use(tronsauce())
   .connect()
