ef0299be893b7ab1ce396718e9725b4610fc1c48
Steve Kellock <steve@kellock.ca>
2016-08-13 01:02:41 -0400

    Adds a state values renderer. (#109)

--- packages/reactotron-app/App/Shared/MakeTable.js
+++ packages/reactotron-app/App/Shared/MakeTable.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { map, toPairs } from 'ramda'
 import Colors from '../Theme/Colors'
+import { map, toPairs, identity, isNil, T, equals, cond, always } from 'ramda'
 
 const Styles = {
   row: {
-    fontSize: 14,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     WebkitUserSelect: 'all'
   },
   key: {
-    minWidth: 150,
-    width: 150,
+    width: '25%',
+    paddingRight: 10,
     wordBreak: 'break-all',
-    color: Colors.matteBlack
+    textAlign: 'right',
+    color: Colors.text
   },
   value: {
     flex: 1,
@@ -23,10 +23,18 @@ const Styles = {
 }
 
 const makeRow = ([key, value]) => {
+  const textValue = cond([
+    [isNil, always('¯\\_(ツ)_/¯')],
+    [x => typeof x === 'boolean', always(value ? 'true' : 'false')],
+    [T, identity]
+  ])(value)
+
   return (
     <div key={key} style={Styles.row}>
       <div style={Styles.key}>{key}</div>
-      <div style={Styles.value}>{value}</div>
+      <div style={Styles.value}>
+        {textValue}
+      </div>
     </div>
   )
 }
