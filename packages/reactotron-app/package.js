7b1fd45f797b57f3c62d13c25756e51cfd3857b0
Steve Kellock <steve@kellock.ca>
2016-08-18 14:25:09 -0400

    Adds a codesigning script.

--- packages/reactotron-app/package.js
+++ packages/reactotron-app/package.js
@@ -21,6 +21,7 @@ const DEFAULT_OPTS = {
   dir: './',
   name: appName,
   asar: shouldUseAsar,
+  'app-bundle-id': 'com.reactotron.app',
   ignore: [
     '^/test($|/)',
     '^/release($|/)',
@@ -47,7 +48,7 @@ if (version) {
   // use the same version as the currently-installed electron-prebuilt
   exec('npm list electron-prebuilt --dev', (err, stdout) => {
     if (err) {
-      DEFAULT_OPTS.version = '1.2.0'
+      DEFAULT_OPTS.version = '1.3.2'
     } else {
       DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '')
     }
