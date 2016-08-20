5a1246831251ef41b3a451e2924f8a37393a46d7
Steve Kellock <steve@kellock.ca>
2016-08-19 21:09:55 -0400

    Adds an always on top menu item. (#137)

--- packages/reactotron-app/main.development.js
+++ packages/reactotron-app/main.development.js
@@ -21,6 +21,15 @@ app.on('ready', () => {
     titleBarStyle: 'hidden'
   })
 
+  // toggle the menu on top/bottom
+  function toggleAlwaysOnTop () {
+    const nextValue = !mainWindow.isAlwaysOnTop()
+    mainWindow.setAlwaysOnTop(nextValue)
+    // fragile way of getting the Always On Top
+    const menuItem = menu.items[3].submenu.items[0]
+    menuItem.checked = nextValue
+  }
+
   mainWindow.loadURL(`file://${__dirname}/App/App.html`)
 
   mainWindow.webContents.on('did-finish-load', () => {
@@ -35,116 +44,51 @@ app.on('ready', () => {
   if (process.platform === 'darwin') {
     template = [{
       label: 'Reactotron',
-      submenu: [{
-        label: 'About Reactotron',
-        selector: 'orderFrontStandardAboutPanel:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Services',
-        submenu: []
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Hide Reactotron',
-        accelerator: 'Command+H',
-        selector: 'hide:'
-      }, {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        selector: 'hideOtherApplications:'
-      }, {
-        label: 'Show All',
-        selector: 'unhideAllApplications:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click () {
-          app.quit()
-        }
-      }]
+      submenu: [
+        { label: 'About Reactotron', selector: 'orderFrontStandardAboutPanel:' },
+        { type: 'separator' },
+        { label: 'Services', submenu: [] },
+        { type: 'separator' },
+        { label: 'Hide Reactotron', accelerator: 'Command+H', selector: 'hide:' },
+        { label: 'Hide Others', accelerator: 'Command+Shift+H', selector: 'hideOtherApplications:' },
+        { label: 'Show All', selector: 'unhideAllApplications:' },
+        { type: 'separator' },
+        { label: 'Quit', accelerator: 'Command+Q', click () { app.quit() } }
+      ]
     }, {
       label: 'Edit',
-      submenu: [{
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      }, {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      }, {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      }, {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      }, {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      }]
+      submenu: [
+        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'Command+A', selector: 'selectAll:' }
+      ]
     }, {
       label: 'View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: 'Reload',
-        accelerator: 'Command+R',
-        click () {
-          mainWindow.webContents.reload()
-        }
-      }, {
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click () {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen())
-        }
-      }, {
-        label: 'Toggle Developer Tools',
-        accelerator: 'Alt+Command+I',
-        click () {
-          mainWindow.toggleDevTools()
-        }
-      }] : [{
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click () {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen())
-        }
-      }]
+      submenu: (process.env.NODE_ENV === 'development') ? [
+        { label: 'Reload', accelerator: 'Command+R', click () { mainWindow.webContents.reload() } },
+        { label: 'Toggle Developer Tools', accelerator: 'Alt+Command+I', click () { mainWindow.toggleDevTools() } }
+      ] : [
+        { label: 'Toggle Full Screen', accelerator: 'Ctrl+Command+F', click () { mainWindow.setFullScreen(!mainWindow.isFullScreen()) } }
+      ]
     }, {
       label: 'Window',
-      submenu: [{
-        label: 'Minimize',
-        accelerator: 'Command+M',
-        selector: 'performMiniaturize:'
-      }, {
-        label: 'Close',
-        accelerator: 'Command+W',
-        selector: 'performClose:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Bring All to Front',
-        selector: 'arrangeInFront:'
-      }]
+      submenu: [
+        { type: 'checkbox', label: 'Always On Top', click () { toggleAlwaysOnTop() } },
+        { type: 'separator' },
+        { label: 'Minimize', accelerator: 'Command+M', selector: 'performMiniaturize:' },
+        { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
+        { type: 'separator' },
+        { label: 'Bring All to Front', selector: 'arrangeInFront:' }
+      ]
     }, {
       label: 'Help',
-      submenu: [{
-        label: 'Visit on Github',
-        click () {
-          shell.openExternal('https://github.com/reactotron/reactotron')
-        }
-      }]
+      submenu: [
+        { label: 'Visit on Github', click () { shell.openExternal('https://github.com/reactotron/reactotron') } }
+      ]
     }]
 
     menu = Menu.buildFromTemplate(template)
@@ -152,51 +96,24 @@ app.on('ready', () => {
   } else {
     template = [{
       label: '&File',
-      submenu: [{
-        label: '&Open',
-        accelerator: 'Ctrl+O'
-      }, {
-        label: '&Close',
-        accelerator: 'Ctrl+W',
-        click () {
-          mainWindow.close()
-        }
-      }]
+      submenu: [
+        { label: '&Open', accelerator: 'Ctrl+O' },
+        { label: '&Close', accelerator: 'Ctrl+W', click () { mainWindow.close() } }
+      ]
     }, {
       label: '&View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: '&Reload',
-        accelerator: 'Ctrl+R',
-        click () {
-          mainWindow.webContents.reload()
-        }
-      }, {
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click () {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen())
-        }
-      }, {
-        label: 'Toggle &Developer Tools',
-        accelerator: 'Alt+Ctrl+I',
-        click () {
-          mainWindow.toggleDevTools()
-        }
-      }] : [{
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click () {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen())
-        }
-      }]
+      submenu: (process.env.NODE_ENV === 'development') ? [
+        { label: '&Reload', accelerator: 'Ctrl+R', click () { mainWindow.webContents.reload() } },
+        { label: 'Toggle &Developer Tools', accelerator: 'Alt+Ctrl+I', click () { mainWindow.toggleDevTools() } }
+      ] : [
+        { type: 'checkbox', label: 'Always On Top', click () { toggleAlwaysOnTop(this) } },
+        { label: 'Toggle &Full Screen', accelerator: 'F11', click () { mainWindow.setFullScreen(!mainWindow.isFullScreen()) } }
+      ]
     }, {
       label: 'Help',
-      submenu: [{
-        label: 'Visit on Github',
-        click () {
-          shell.openExternal('https://github.com/reactotron/reactotron')
-        }
-      }]
+      submenu: [
+        { label: 'Visit on Github', click () { shell.openExternal('https://github.com/reactotron/reactotron') } }
+      ]
     }]
     menu = Menu.buildFromTemplate(template)
     mainWindow.setMenu(menu)
