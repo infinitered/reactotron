d60cc8c844d5454146d31bb80df7d3164cc759ef
Steve Kellock <steve@kellock.ca>
2016-08-22 00:14:04 -0400

    Adds a publish script.

---
+++ scripts/publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+echo "Pushing All Y'all Up To NPM"
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+cd packages/reactotron-core-client
+npm run build
+npm publish
+cd ../reactotron-apisauce
+npm run build
+npm publish
+cd ../reactotron-redux
+npm run build
+npm publish
+cd ../reactotron-react-js
+npm run build
+npm publish
+cd ../reactotron-react-native
+npm run build
+npm publish
+cd ../reactotron-core-server
+npm run build
+npm publish
+cd ../reactotron-cli
+npm run build
+npm publish
+cd ../..
