399e19ef92b0472683b70b5d07ae10c8eaf77bec
Steve Kellock <steve@kellock.ca>
2016-08-23 17:13:39 -0400

    Removes the legacy directory.

--- legacy/examples/ReactDomExample/App/Actions/Types.js
+++
@@ -1,6 +0,0 @@
-export default {
-  STARTUP: Symbol('STARTUP'), // Symbols are also supported
-  GITHUB_REQUEST: 'GITHUB_REQUEST',
-  GITHUB_RECEIVE: 'GITHUB_RECEIVE',
-  GITHUB_FAILURE: 'GITHUB_FAILURE'
-}
