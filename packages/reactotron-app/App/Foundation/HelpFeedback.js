e93f24240fdf23e317e4a0c6cb4dfc00e670ca33
Steve Kellock <steve@kellock.ca>
2016-08-23 17:53:05 -0400

    Updates urls to github.

--- packages/reactotron-app/App/Foundation/HelpFeedback.js
+++ packages/reactotron-app/App/Foundation/HelpFeedback.js
@@ -45,7 +45,7 @@ class HelpFeedback extends Component {
 
   constructor (props) {
     super(props)
-    this.openRepo = () => shell.openExternal('https://github.com/reactotron/reactotron/tree/next')
+    this.openRepo = () => shell.openExternal('https://github.com/reactotron/reactotron')
     this.feedback = () => shell.openExternal('https://github.com/reactotron/reactotron/issues/new')
     this.checkUpdates = () => shell.openExternal('https://github.com/reactotron/reactotron/releases')
     this.twitter = () => shell.openExternal('https://twitter.com/reactotron')
