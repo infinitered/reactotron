9d2bbee2675690f70794db39f866cc624ba7874a
Steve Kellock <steve@kellock.ca>
2016-08-24 09:31:36 -0400

    Adds a symbol polyfill to react native demo for compat.

--- packages/demo-react-native/index.android.js
+++ packages/demo-react-native/index.android.js
@@ -1,4 +1,5 @@
 // imported from a file to ensure the connect statement gets run first!
+import 'es6-symbol/implement'
 import './App/Config/ReactotronConfig'
 import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
