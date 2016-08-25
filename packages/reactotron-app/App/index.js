f8deae3161485087ce6af9a6cd621c4114cfe2bd
Kenneth Shaw <kenneth.shaw@knq.io>
2016-08-25 07:47:10 +0700

    reactotron-app: fix case sensitive file paths (#170)
    This fixes the incorrect case in a few file paths that prevented
    electron/webpack from loading files on case sensitive file systems (ie,
    those on Unix systems).

--- packages/reactotron-app/App/index.js
+++ packages/reactotron-app/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './Foundation/App'
-import './App.global.css'
+import './app.global.css'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
