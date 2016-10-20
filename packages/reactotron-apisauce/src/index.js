b1f9fb296f76016a42b62724a69be1b7297fe5b8
Patrick Smith <pjs482@gmail.com>
2016-10-20 11:59:11 -0400

    Show request params in Reactotron App (#218)
    * Appease the linting gods
    
    Fixes linting error:
    
        reactotron/packages/reactotron-app/App/Commands/BenchmarkReportCommand.js:109:11: Expected indentation of 8 space characters but found 10.
    
    * Pass along request params in apisauce middleware
    
    * Show request params, if params exist

--- packages/reactotron-apisauce/src/index.js
+++ packages/reactotron-apisauce/src/index.js
@@ -6,11 +6,14 @@ const convertResponse = (source) => {
   const method = RS.dotPath('config.method', source)
   const requestData = RS.dotPath('config.data', source)
   const requestHeaders = RS.dotPath('config.headers', source)
+  const requestParams = RS.dotPath('config.params', source)
   const duration = RS.dotPath('duration', source)
   const status = RS.dotPath('status', source)
   const body = RS.dotPath('data', source)
   const responseHeaders = RS.dotPath('headers', source)
-  const request = { url, method, data: requestData, headers: requestHeaders }
+  const request = {
+    url, method, data: requestData, headers: requestHeaders, params: requestParams
+  }
   const response = { body, status, headers: responseHeaders }
 
   return [ request, response, duration ]
