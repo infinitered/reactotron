d2bd38457a474d6e2ca9c98981f126e1446333c5
Richard Evans <rmevans9@gmail.com>
2016-12-25 20:09:21 -0500

    Basic Immutable JS support

--- packages/reactotron-redux/src/get-subscription-values.js
+++ packages/reactotron-redux/src/get-subscription-values.js
@@ -1,13 +1,16 @@
 import R from 'ramda'
 import RS from 'ramdasauce'
+import getCleanedState from './state-cleaner'
 
 // fishes out the values for the subscriptions in state and returns them
-export default (subscriptions, state) =>
-  R.pipe(
+export default (subscriptions, state) => {
+  const cleanedState = getCleanedState(state)
+
+  return R.pipe(
     R.filter(RS.endsWith('.*')),
     R.map((key) => {
       const keyMinusWildcard = R.slice(0, -2, key)
-      const value = RS.dotPath(keyMinusWildcard, state)
+      const value = RS.dotPath(keyMinusWildcard, cleanedState)
       if (R.is(Object, value) && !RS.isNilOrEmpty(value)) {
         return R.pipe(
           R.keys,
@@ -21,5 +24,6 @@ export default (subscriptions, state) =>
     R.reject(RS.endsWith('.*')),
     R.uniq,
     R.sortBy(R.identity),
-    R.map(key => ({ path: key, value: RS.dotPath(key, state) }))
+    R.map(key => ({ path: key, value: RS.dotPath(key, cleanedState) }))
   )(subscriptions)
+}
