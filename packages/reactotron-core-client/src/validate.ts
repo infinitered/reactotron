bb180beb7478eca429d857d049af93e4c6fb1b79
rmevans9 <rmevans9@gmail.com>
2017-07-20 22:21:37 -0400

    Better Types for core-client (#484)

--- packages/reactotron-core-client/src/validate.ts
+++ packages/reactotron-core-client/src/validate.ts
@@ -1,16 +1,18 @@
 import R from 'ramda'
 import RS from 'ramdasauce'
+import WebSocket from 'ws'
+import { Options } from './index'
 
-const isCreateSocketValid = (createSocket) => !R.isNil(createSocket)
+const isCreateSocketValid = (createSocket: (path: string) => WebSocket) => !R.isNil(createSocket)
 const isHostValid = R.allPass([R.complement(RS.isNilOrEmpty), R.is(String)])
 const isPortValid = R.allPass([R.complement(R.isNil), R.is(Number), RS.isWithin(1, 65535)])
-const onCommandValid = (fn) => typeof fn === 'function'
+const onCommandValid = (fn: (cmd: string) => any) => typeof fn === 'function'
 
 /**
  * Ensures the options are sane to run this baby.  Throw if not.  These
  * are basically sanity checks.
  */
-const validate = (options: any) => {
+const validate = (options: Options) => {
   const { createSocket, host, port, onCommand } = options
 
   if (!isCreateSocketValid(createSocket)) throw new Error('invalid createSocket function')
