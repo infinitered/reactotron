e4f6d211496800beecfa843884240ff415079b76
Steve Kellock <steve@kellock.ca>
2017-10-10 09:01:51 -0400

    Cleans up some older stuff we're not maintaining.  <3 (#526)

--- soon/react-boilerplate/copy-internal-deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp ../reactotron-react-js/dist/index.js ./node_modules/reactotron-react-js/dist/index.js
-cp ../reactotron-core-client/dist/index.js ./node_modules/reactotron-core-client/dist/index.js
-cp ../reactotron-redux/dist/index.js ./node_modules/reactotron-redux/dist/index.js
-cp ../reactotron-redux-saga/dist/index.js ./node_modules/reactotron-redux-saga/dist/index.js
