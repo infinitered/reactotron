fb3aea7c3a8a062c46bf7a81f5fbec811209e2cd
Vyacheslav Salakhutdinov <megazoll@gmail.com>
2017-12-14 14:26:55 +0300

    Fix warning when source effect has no effect

--- packages/reactotron-redux-saga/src/saga-monitor.js
+++ packages/reactotron-redux-saga/src/saga-monitor.js
@@ -114,24 +114,26 @@ export default (reactotron, options) => {
         if (isNil(sourceEffectInfo)) return
 
         let extra = null
-        switch (sourceEffectInfo.name) {
-          case CALL:
-            extra = sourceEffectInfo.effect[sourceEffectInfo.name].args
-            break
+        if (sourceEffectInfo.effect) {
+          switch (sourceEffectInfo.name) {
+            case CALL:
+              extra = sourceEffectInfo.effect[sourceEffectInfo.name].args
+              break
 
-          case PUT:
-            extra = sourceEffectInfo.effect[sourceEffectInfo.name].action
-            break
+            case PUT:
+              extra = sourceEffectInfo.effect[sourceEffectInfo.name].action
+              break
 
-          // children handle this
-          case RACE:
-            break
+            // children handle this
+            case RACE:
+              break
 
-          // TODO: More of customizations needed here
+            // TODO: More of customizations needed here
 
-          default:
-            extra = sourceEffectInfo.effect[sourceEffectInfo.name]
-            break
+            default:
+              extra = sourceEffectInfo.effect[sourceEffectInfo.name]
+              break
+          }
         }
         // assemble the structure
         children.push({
