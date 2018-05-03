12d0278567c2a1fc74d4779ca143a45b5e6bfdd4
Steve Kellock <steve@kellock.ca>
2018-05-03 07:34:20 -0400

    🔥 Removes ramda from rollup

--- packages/reactotron-core-client/rollup.config.js
+++ packages/reactotron-core-client/rollup.config.js
@@ -2,7 +2,6 @@ import commonjs from 'rollup-plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
 import uglify from 'rollup-plugin-uglify'
 import sourcemaps from 'rollup-plugin-sourcemaps'
-import ramda from 'rollup-plugin-ramda'
 
 export default {
   input: 'compiled/reactotron-core-client.js',
@@ -12,7 +11,6 @@ export default {
       include: 'node_modules/**',
       ignoreGlobals: false,
     }),
-    ramda(),
     sourcemaps(),
     uglify(),
     filesize(),
@@ -21,21 +19,5 @@ export default {
     file: 'dist/reactotron-core-client.js',
     format: 'cjs',
     exports: 'named',
-  },
-  external: [
-    'ramda/src/allPass',
-    'ramda/src/complement',
-    'ramda/src/contains',
-    'ramda/src/forEach',
-    'ramda/src/has',
-    'ramda/src/head',
-    'ramda/src/is',
-    'ramda/src/isEmpty',
-    'ramda/src/isNil',
-    'ramda/src/keys',
-    'ramda/src/last',
-    'ramda/src/length',
-    'ramda/src/merge',
-    'ramda/src/tail',
-  ],
+  }
 }
