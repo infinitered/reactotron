4ef8498cd6ff73688a85a3dcd7b14d8f153862fb
Steve Kellock <steve@kellock.ca>
2018-05-08 08:03:26 -0400

    ♻️ Drops the sCU from the <Command />

--- packages/reactotron-app/App/Shared/Command.js
+++ packages/reactotron-app/App/Shared/Command.js
@@ -1,86 +1,85 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Colors from '../Theme/Colors'
-import AppStyles from '../Theme/AppStyles'
-import Timestamp from '../Shared/Timestamp'
-import { observer } from 'mobx-react'
-import { merge, equals } from 'ramda'
-import CommandToolbar from './CommandToolbar'
-import DisplayIcon from 'react-icons/lib/md/label'
+import { observer } from "mobx-react"
+import PropTypes from "prop-types"
+import React, { Component } from "react"
+import DisplayIcon from "react-icons/lib/md/label"
+import Timestamp from "../Shared/Timestamp"
+import AppStyles from "../Theme/AppStyles"
+import Colors from "../Theme/Colors"
+import CommandToolbar from "./CommandToolbar"
 
-const IconOpen = require('react-icons/lib/md/expand-more')
-const IconClosed = require('react-icons/lib/md/chevron-right')
+const IconOpen = require("react-icons/lib/md/expand-more")
+const IconClosed = require("react-icons/lib/md/chevron-right")
 
 const Styles = {
   container: {
     ...AppStyles.Layout.hbox,
     marginTop: 0,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     borderBottom: `1px solid ${Colors.line}`,
   },
   containerOpen: {
-    backgroundColor: Colors.backgroundSubtleLight
+    backgroundColor: Colors.backgroundSubtleLight,
   },
   icon: {
-    color: Colors.backgroundHighlight
+    color: Colors.backgroundHighlight,
   },
   body: {
     ...AppStyles.Layout.vbox,
-    marginLeft: 0
+    marginLeft: 0,
   },
   topRow: {
     ...AppStyles.Layout.hbox,
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    padding: '15px 20px',
-    cursor: 'pointer'
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    padding: "15px 20px",
+    cursor: "pointer",
   },
   title: {
-    textAlign: 'left',
-    width: 168
+    textAlign: "left",
+    width: 168,
   },
   titleText: {
-    color: Colors.tag
+    color: Colors.tag,
   },
   titleTextInverse: {
     backgroundColor: Colors.tag,
     color: Colors.tagComplement,
     borderRadius: 4,
-    padding: '4px 8px'
+    padding: "4px 8px",
   },
   displayIcon: {
     marginRight: 4,
-    marginBottom: 4
+    marginBottom: 4,
   },
   displayIconSize: 16,
   preview: {
     color: Colors.highlight,
-    textAlign: 'left',
+    textAlign: "left",
     paddingRight: 16,
-    overflow: 'hidden',
+    overflow: "hidden",
     WebkitLineClamp: 3,
-    WebkitBoxOrient: 'vertical',
-    textOverflow: 'ellipsis',
-    display: '-webkit-box',
+    WebkitBoxOrient: "vertical",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
     flex: 1,
-    wordBreak: 'break-all'
+    wordBreak: "break-all",
   },
   duration: {
     color: Colors.foregroundDark,
-    paddingRight: 10
+    paddingRight: 10,
   },
   timestamp: {
     color: Colors.foregroundDark,
-    paddingRight: 10
+    paddingRight: 10,
   },
   spacer: {
-    flex: 1
+    flex: 1,
   },
   children: {
-    overflow: 'hidden',
-    animation: 'fade-up 0.25s',
-    willChange: 'transform opacity',
-    padding: '0 40px 30px 40px'
+    overflow: "hidden",
+    animation: "fade-up 0.25s",
+    willChange: "transform opacity",
+    padding: "0 40px 30px 40px",
   },
 }
 
@@ -91,40 +90,40 @@ class Command extends Component {
     title: PropTypes.string.isRequired,
     preview: PropTypes.string,
     subtitle: PropTypes.string,
-    duration: PropTypes.number
+    duration: PropTypes.number,
   }
 
   state = {
-    isOpen: false
+    isOpen: false,
   }
 
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      isOpen: props.startsOpen || false
+      isOpen: props.startsOpen || false,
     }
     this.handleToggleOpen = this.handleToggleOpen.bind(this)
   }
 
-  shouldComponentUpdate (nextProps, nextState) {
-    return !(equals(nextProps, this.props) && equals(this.state, nextState))
-  }
+  // shouldComponentUpdate (nextProps, nextState) {
+  //   return !(equals(nextProps, this.props) && equals(this.state, nextState))
+  // }
 
-  handleToggleOpen () {
+  handleToggleOpen() {
     this.setState({ isOpen: !this.state.isOpen })
   }
 
-  render () {
+  render() {
     const { isOpen } = this.state
     const { command, children, title, preview, deltaTime } = this.props
     const { important, type } = command
-    const isDisplay = type === 'display'
+    const isDisplay = type === "display"
     const { date } = command
-    const titleTextStyle = merge(Styles.titleText, important ? Styles.titleTextInverse : {})
+    const titleTextStyle = { ...Styles.titleText, ...(important && Styles.titleTextInverse) }
     const topRowStyle = Styles.topRow
     const timestampStyle = Styles.timestamp
     const Icon = isOpen ? IconOpen : IconClosed
-    const containerStyles = merge(Styles.container, isOpen && Styles.containerOpen)
+    const containerStyles = { ...Styles.container, ...(isOpen && Styles.containerOpen) }
     return (
       <div style={containerStyles}>
         <div style={Styles.body}>
