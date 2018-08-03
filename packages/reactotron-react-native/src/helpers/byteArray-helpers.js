eac2289f7fc138655b0850cf4d94ce5c08557bd2
Boris Conforty <boris@conforty.net>
2018-08-03 14:30:42 +0200

    Added byteArray to string conversion helper (supports gzip)

---
+++ packages/reactotron-react-native/src/helpers/byteArray-helpers.js
@@ -0,0 +1,30 @@
+import { decode as utf8Decode } from "./utf8-helpers"
+
+const pako = require("pako")
+
+export default {
+  byteArrayToStr: (byteArray, method = "auto") => {
+    let actualMethod
+    if (method === "auto") {
+      if (byteArray.length > 1) {
+        // cf. http://www.zlib.org/rfc-gzip.html
+        const [ID1, ID2] = byteArray
+        if (ID1 === 31 && ID2 === 139) {
+          actualMethod = "gzip"
+        } else {
+          actualMethod = "string"
+        }
+      } else {
+        actualMethod = method
+      }
+    }
+    switch (actualMethod) {
+      case "string":
+        return utf8Decode(byteArray)
+      case "gzip":
+        return pako.inflate(byteArray, { to: "string" })
+      default:
+        throw new Error(`Invalid byte array to string method: ${method}`)
+    }
+  },
+}
