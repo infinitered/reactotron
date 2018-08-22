e41f96f933963f2a8fd9402ab8cb65d9eac2151b
Steve Kellock <steve@kellock.ca>
2018-08-22 08:05:01 -0400

    ✨ captures additional client info upon connecting

--- packages/reactotron-server/src/server/apollo/schema.ts
+++ packages/reactotron-server/src/server/apollo/schema.ts
@@ -4,6 +4,32 @@ const schema = gql`
   type Connection {
     clientId: String!
     platform: String
+    name: String
+    environment: String
+    reactotronLibraryName: String
+    reactotronLibraryVersion: String
+    platformVersion: String
+    osRelease: String
+    model: String
+    serverHost: String
+    forceTouch: Boolean
+    interfaceIdiom: String
+    systemName: String
+    uiMode: String
+    serial: String
+    androidId: String
+    reactNativeVersion: String
+    screenWidth: Int
+    screenHeight: Int
+    screenScale: Int
+    screenFontScale: Int
+    windowWidth: Int
+    windowHeight: Int
+    windowScale: Int
+    windowFontScale: Int
+    reactotronCoreClientVersion: String
+    address: String
+    id: Int
   }
 
   type Command {
