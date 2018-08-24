76712b7add7b6f56d40abe80a125d70fd051893d
Steve Kellock <steve@kellock.ca>
2018-08-24 10:03:32 -0400

    ✅ adds some tests for v3

--- packages/reactotron-server/src/server/reactotron/commandHandler.ts
+++ packages/reactotron-server/src/server/reactotron/commandHandler.ts
@@ -1,12 +1,26 @@
 import { commands } from "../datastore"
 import { messaging, MessageTypes } from "../messaging"
+import { Command } from "../schema"
 
-function onCommand(command) {
+/**
+ * Fires when an event arrives from a reactotron client.
+ *
+ * This is responsible for handing the command off to the datastore and
+ * signaling the GraphQL subscription service that it happened.
+ *
+ * @param command A command sent from the client.
+ */
+export function onCommand(command: Command) {
   commands.addCommand(command)
 
   messaging.publish(MessageTypes.COMMAND_ADDED, command)
 }
 
+/**
+ * Registers handlers for the events that reactotron fires.
+ *
+ * @param reactotronServer The reactotron-core-server instance
+ */
 export function addEventHandlers(reactotronServer) {
   reactotronServer.on("command", onCommand)
 }
