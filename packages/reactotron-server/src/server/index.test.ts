5770b8e16eda5be73edbeb820fd1297ab8ac1a62
Ian Outterside <ian@komododigital.co.uk>
2018-08-31 10:12:04 +0100

    Refactor setup so that apollo server is configured before listen is called on express.

--- packages/reactotron-server/src/server/index.test.ts
+++ packages/reactotron-server/src/server/index.test.ts
@@ -1,36 +1,39 @@
 import test from "ava"
 import * as request from "request-promise-native"
-import { httpServerInstance, apolloServerInstance } from './'
+import { httpServerInstance, apolloServerInstance, startServersListening } from "./"
 
 // Tests in here are ran in serial due to the webserver binding to port 4000 (concurrency results in EADDRINUSE error)
 
-let app, httpServer;
+let appServer, apolloServer;
 
 test.serial("Can create a http server to serve react app", async t => {
-
-  let appServer;
 
   t.notThrows(() => {
     appServer = httpServerInstance()
   })
 
   // Not sure if there is a way to nicely destructure here, this does for now
-  app = appServer.app;
-  httpServer = appServer.httpServer;
+  let httpServer = appServer.httpServer;
 
-  t.true(httpServer !== null && typeof httpServer === "object")
+  t.true(httpServer !== null && typeof httpServer === "object")  
+}) 
+
+test.serial("Can create an apollo server to interact with react app", async t => {
+  
+  const { app, httpServer } = appServer
+  apolloServer = await apolloServerInstance(app, httpServer)
+
+  t.true(apolloServer !== null && typeof apolloServer === "object")
+  t.true(apolloServer.graphqlPath === '/graphql')
+}) 
+
+test.serial("Does serve react app and apollo pubsub", async t => {
+
+  startServersListening(appServer, apolloServer)
 
   const responseString = await request("http://localhost:4000").catch(err => {
     t.is(err, null, "request to http server has errored")
   })
 
   t.true(responseString.includes("the new home of reactotron"))
-}) 
-
-test.serial("Can create an apollo server to interact with react app", async t => {
-  
-  const apolloServer = await apolloServerInstance(app, httpServer)
-
-  t.true(apolloServer !== null && typeof apolloServer === "object")
-  t.true(apolloServer.graphqlPath === '/graphql')
-}) 
+})
