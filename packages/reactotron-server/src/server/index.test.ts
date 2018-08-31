63aa0e2afec0bce7443f1ee5a684c8300f80c3b9
Ian Outterside <ian@komododigital.co.uk>
2018-08-31 10:47:14 +0100

    Adding GraphQL client test

--- packages/reactotron-server/src/server/index.test.ts
+++ packages/reactotron-server/src/server/index.test.ts
@@ -1,8 +1,11 @@
 import test from "ava"
 import * as request from "request-promise-native"
+import { GraphQLClient } from "graphql-request"
 import { httpServerInstance, apolloServerInstance, startServersListening } from "./"
 
 // Tests in here are ran in serial due to the webserver binding to port 4000 (concurrency results in EADDRINUSE error)
+// I really want to find a way to refactor out the serials in here. Perhaps after calling "startServersListening" we dont need them any more?
+// Im new to ava so not sure what the best solution is here
 
 let appServer, apolloServer;
 
@@ -37,3 +40,25 @@ test.serial("Does serve react app and apollo pubsub", async t => {
 
   t.true(responseString.includes("the new home of reactotron"))
 })
+
+test.serial("Does return reactotron current connection details", async t => {
+
+  // @ts-nocheck
+  const client = new GraphQLClient('http://localhost:4000/graphql')
+
+  const query = `{
+    connections {
+      platform
+      name
+    }
+  }`
+
+  const data = await client.request(query).catch(err => console.log(err))
+
+  console.log(data);
+
+  t.deepEqual({
+    connections: []
+  }, data)
+
+});
