33057818e91a1143e7ddb8980d130274e42e68a6
Richard Evans <rmevans9@gmail.com>
2018-09-05 20:07:44 -0400

    Add build statements to make things a little easier

--- setupV3ServerLinks.sh
+++ setupV3ServerLinks.sh
@@ -1,11 +1,14 @@
 # This is a terrible idea but it works for now. One day hopefully we will have a better solution but this works for now
 cd packages/reactotron-core-plugin
+yarn build
 sh ./run-links.sh
 
 cd ../reactotron-plugin-legacy
+yarn build
 sh ./run-links.sh
 
 cd ../reactotron-plugin-example
+yarn build
 sh ./run-links.sh
 
 cd ../reactotron-server
