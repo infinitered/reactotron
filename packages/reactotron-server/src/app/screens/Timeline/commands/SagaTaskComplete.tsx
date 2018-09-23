a3728a7f9662f718c2e341de0fdc9fa5f392648a
Ian Outterside <ianoshorty@gmail.com>
2018-09-23 17:12:40 +0100

    Add Saga Timeline Componenet (#832)
    Adds a saga timeline component in the same style as API response. Adds some basic styling to match existing app.
    
    NOTE: This includes changes made in https://github.com/infinitered/reactotron/pull/831 so suggest reviewing that PR first.

--- packages/reactotron-server/src/app/screens/Timeline/commands/SagaTaskComplete.tsx
+++ packages/reactotron-server/src/app/screens/Timeline/commands/SagaTaskComplete.tsx
@@ -4,14 +4,14 @@ interface Props {
   command: any // TODO: Better typing yo
 }
 
-export class ApiResponse extends React.Component<Props> {
+export class SagaTaskComplete extends React.Component<Props> {
   render() {
     const { command } = this.props
-
+    
     return <div className="bg-content border flex flex-row justify-between p-6">
       <span>{command.date}</span>
-      <span className="text-orange">API RESPONSE</span>
-      <span>{command.payload.request.url}</span>
+      <span className="text-orange">SAGA</span>
+      <span>{command.type}</span>
       <span>&#62;</span>
     </div>
   }
