a51c18cddf473b9027685d722545895c9fff78e6
Ian Outterside <ianoshorty@gmail.com>
2018-10-02 20:47:55 +0100

    Adding more styling to APIResponse and handling expanding of different data points

--- packages/reactotron-server/src/app/commands/ApiResponse.tsx
+++ packages/reactotron-server/src/app/commands/ApiResponse.tsx
@@ -1,32 +1,90 @@
 import React from "react"
 import { Command } from "reactotron-core-ui"
 
-export function ApiResponseTimeline({ command }: { command: Command }) {
-  console.log(command)
-  let contents = JSON.stringify(command.payload.request);
-  return <div>
-      <span className="text-highlight">{command.payload.request.url}</span>
+interface Props {
+  command: Command
+}
+
+interface State {
+  expanded: null | string
+}
+
+export class ApiResponseTimeline extends React.Component <Props, State> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      expanded: null,
+    }
+  }
+
+  _renderContentsBlock() {
+    let contents
+
+    switch (this.state.expanded) {
+      case "response":
+        contents = JSON.stringify(this.props.command.payload.response.body)
+        break
+      case "responseHeaders":
+        contents = JSON.stringify(this.props.command.payload.response.headers)
+        break
+      case "requestHeaders":
+        contents = JSON.stringify(this.props.command.payload.request.headers)
+        break
+      default:
+        contents = null
+        break
+    }
+
+    if (!contents) {
+      return null;
+    }
+
+    return <div className="mx-2 my-2">
+      <span>{contents}</span>
+    </div>;
+  }
+
+  render() {
+    return (
       <div>
-        <span>Status Code</span>
-        <span>200</span>
-        <span>Method</span>
-        <span>GET</span>
-        <span>Duration (ms)</span>
-        <span>16</span>
+        <div className="mx-2 my-2">
+          <span className="text-highlight">{this.props.command.payload.request.url}</span>
+        </div>
+        <div className="mx-2 my-2 flex flex-wrap">
+          <span className="w-1/2">Status Code</span>
+          <span className="w-1/2 text-highlight">{this.props.command.payload.response.status}</span>
+          <span className="w-1/2">Method</span>
+          <span className="w-1/2 text-subtle uppercase">{this.props.command.payload.request.method}</span>
+          <span className="w-1/2">Duration (ms)</span>
+          <span className="w-1/2 text-highlight">{this.props.command.payload.duration}</span>
+        </div>
+        <div>
+          <button
+            className="bg-expandButton hover:bg-expandButtonHover text-default py-2 px-4 mx-2 rounded bg-expandButtonSelect"
+            onClick={() => this.setState({ expanded: "response" })}
+          >
+            Response
+          </button>
+          <button
+            className="bg-expandButton hover:bg-expandButtonHover text-default py-2 px-4 mx-2 rounded"
+            onClick={() => this.setState({ expanded: "responseHeaders" })}
+          >
+            Response Headers
+          </button>
+          <button
+            className="bg-expandButton hover:bg-expandButtonHover text-default py-2 px-4 mx-2 rounded"
+            onClick={() => this.setState({ expanded: "requestHeaders" })}
+          >
+            Request Headers
+          </button>
+        </div>
+        {this._renderContentsBlock()}
       </div>
-      <div>
-        <button className="bg-expandButton hover:bg-expandButtonHover text-default py-2 px-4 rounded bg-expandButtonSelect">Response</button>
-        <button className="bg-expandButton hover:bg-expandButtonHover text-default py-2 px-4 rounded">Response Headers</button>
-        <button className="bg-expandButton hover:bg-expandButtonHover text-default py-2 px-4 rounded">Request Headers</button>
-      </div>
-      <div>
-        <span>
-          {contents}
-        </span>
-      </div>
-  </div>;
+    )
+  }
 }
 
 export function ApiResponsePreview({ command }: { command: Command }) {
-  return <span>{command.payload.request.url}</span>;
+  return <span>{command.payload.request.url}</span>
 }
