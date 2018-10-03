97245950a0d9abc73cb1f8b1a2486dad34347a24
Richard Evans <rmevans9@gmail.com>
2018-10-02 21:57:33 -0400

    First pass at a json tree

--- packages/reactotron-core-ui/src/reactotron-core-ui.ts
+++ packages/reactotron-core-ui/src/reactotron-core-ui.ts
@@ -1,6 +1,7 @@
 import { Text } from "./text"
+import { JsonTree } from "./json-tree"
 
 // Interfaces
-import { Command, TimelineCommandOptions, ReactotronApp } from './reactotron-app';
+import { Command, TimelineCommandOptions, ReactotronApp } from "./reactotron-app"
 
-export { Text, Command, TimelineCommandOptions, ReactotronApp }
+export { Text, JsonTree, Command, TimelineCommandOptions, ReactotronApp }
