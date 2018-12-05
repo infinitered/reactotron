7f9b1a451c00c31f9a98f24466a636866e6fd830
Richard Evans <rmevans9@gmail.com>
2018-12-04 20:04:04 -0500

    chore(dev): Fix script

--- examples/demo-react-native/copy-internal-deps.sh
+++ examples/demo-react-native/copy-internal-deps.sh
@@ -22,34 +22,34 @@ rm -f node_modules/reactotron-apisauce
 # reactotron-core-client
 cd ../../../reactotron-core-client
 yarn run build
-cd ../reactotron/packages/demo-react-native
+cd ../reactotron/examples/demo-react-native
 mkdir -p ./node_modules/reactotron-react-native/node_modules/reactotron-core-client
 cp ../../../reactotron-core-client/dist/reactotron-core-client.js ./node_modules/reactotron-react-native/node_modules/reactotron-core-client/index.js
 
 # reactotron-react-native
 cd ../../../reactotron-react-native
 yarn run build
-cd ../reactotron/packages/demo-react-native
+cd ../reactotron/examples/demo-react-native
 mkdir -p ./node_modules/reactotron-react-native
-cd ../../../reactotron-react-native/dist/index.js ./node_modules/reactotron-react-native/index.js
+cp ../../../reactotron-react-native/dist/index.js ./node_modules/reactotron-react-native/index.js
 
 # reactotron-redux
 cd ../../../reactotron-redux
 yarn run build
-cd ../reactotron/packages/demo-react-native
+cd ../reactotron/examples/demo-react-native
 mkdir -p ./node_modules/reactotron-redux
 cp ../../../reactotron-redux/dist/index.js ./node_modules/reactotron-redux/index.js
 
 # reactotron-apisauce
 cd ../../../reactotron-apisauce
 yarn run build
-cd ../reactotron/packages/demo-react-native
+cd ../reactotron/examples/demo-react-native
 mkdir -p ./node_modules/reactotron-apisauce
 cp ../../../reactotron-apisauce/dist/index.js ./node_modules/reactotron-apisauce/index.js
 
 # reactotron-redux-saga
 cd ../../../reactotron-redux-saga
 yarn run build
-cd ../reactotron/packages/demo-react-native
+cd ../reactotron/examples/demo-react-native
 mkdir -p ./node_modules/reactotron-redux-saga
 cp ../../../reactotron-redux-saga/dist/index.js ./node_modules/reactotron-redux-saga/index.js
