42ce290698f7ee324648349e0aa6373a0ed7416f
rmevans9 <rmevans9@gmail.com>
2018-12-09 19:42:02 -0500

    feat(build): Build process revamp

---
+++ configs/webpack.config.base.js
@@ -0,0 +1,52 @@
+/**
+ * Base webpack config used across other specific configs
+ */
+
+import path from 'path';
+import webpack from 'webpack';
+import { dependencies } from '../package.json';
+
+export default {
+  externals: [...Object.keys(dependencies || {})],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, '..', 'src'),
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: 'commonjs2'
+  },
+
+  /**
+   * Determine the array of extensions that should be used to resolve modules.
+   */
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production'
+    }),
+
+    new webpack.NamedModulesPlugin()
+  ]
+};
