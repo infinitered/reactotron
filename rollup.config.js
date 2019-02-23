f6dbf64825b474223d5d2f2c9bb180dc6b9e3135
Richard Evans <rmevans9@gmail.com>
2019-02-23 14:24:13 -0500

    chore: Update build process to minify everything

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,21 @@
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
+import resolve from "rollup-plugin-node-resolve"
+import babel from "rollup-plugin-babel"
+import filesize from "rollup-plugin-filesize"
+import minify from "rollup-plugin-babel-minify"
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
-    file: 'dist/index.js',
-    format: 'cjs'
+    file: "dist/index.js",
+    format: "cjs",
   },
   plugins: [
-    resolve({ extensions: ['.ts'] }),
-    babel({ extensions: ['.ts'], runtimeHelpers: true })
+    resolve({ extensions: [".ts"] }),
+    babel({ extensions: [".ts"], runtimeHelpers: true }),
+    minify({
+      comments: false,
+    }),
+    filesize(),
   ],
-  external: ['redux']
+  external: ["redux"],
 }
