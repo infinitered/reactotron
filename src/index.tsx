94ed6191525948a929a28d49cf11c053aa1c5769
Richard Evans <rmevans9@gmail.com>
2019-04-30 22:16:04 -0400

    Fix it so it runs right

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
-import React from "react"
+import * as React from "react"
 import { render } from "react-dom"
-import * as App from "./Foundation/App"
+import App from "./Foundation/App"
 import "./app.global.css"
 import * as injectTapEventPlugin from "react-tap-event-plugin"
 injectTapEventPlugin()
 
 const ShutUpTypeScript: any = App
 
-class Test extends React.Compoent {
-    render() {
-        <div>
-            Hello.
-        </div>
-    }
-}
-
-render(<Test />, document.getElementById("root"))
+render(<ShutUpTypeScript />, document.getElementById("root"))
 
 document.addEventListener("dragover", event => event.preventDefault())
 document.addEventListener("drop", event => event.preventDefault())
