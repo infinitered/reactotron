ea4bc341437483c94feff96faf4a13823cfeac6b
rmevans9 <rmevans9@gmail.com>
2019-05-16 22:37:12 -0400

    fix: Always on top causing a javascript error

--- src/menu.js
+++ src/menu.js
@@ -8,6 +8,11 @@ export default class MenuBuilder {
     this.mainWindow = mainWindow
   }
 
+  toggleAlwaysOnTop() {
+    const nextValue = !this.mainWindow.isAlwaysOnTop()
+    this.mainWindow.setAlwaysOnTop(nextValue)
+  }
+
   buildMenu() {
     if (process.env.NODE_ENV === "development" || process.env.DEBUG_PROD === "true") {
       this.setupDevelopmentEnvironment()
@@ -135,8 +140,8 @@ export default class MenuBuilder {
         },
         {
           label: "Toggle Side Menu",
-          click() {
-            mainWindow.webContents.send("toggle-side-menu")
+          click: () => {
+            this.mainWindow.webContents.send("toggle-side-menu")
           },
         },
       ],
@@ -147,8 +152,8 @@ export default class MenuBuilder {
         {
           type: "checkbox",
           label: "Always On Top",
-          click() {
-            mainWindow.toggleAlwaysOnTop()
+          click: () => {
+            this.toggleAlwaysOnTop()
           },
         },
         {
@@ -166,7 +171,7 @@ export default class MenuBuilder {
       submenu: [
         {
           label: "Visit on Github",
-          click() {
+          click: () => {
             shell.openExternal("https://github.com/infinitered/reactotron")
           },
         },
@@ -192,8 +197,8 @@ export default class MenuBuilder {
                 {
                   type: "checkbox",
                   label: "Always On Top",
-                  click() {
-                    mainWindow.toggleAlwaysOnTop(this)
+                  click: () => {
+                    this.toggleAlwaysOnTop()
                   },
                 },
                 {
@@ -222,8 +227,8 @@ export default class MenuBuilder {
                 {
                   type: "checkbox",
                   label: "Always On Top",
-                  click() {
-                    mainWindow.toggleAlwaysOnTop(this)
+                  click: () => {
+                    this.toggleAlwaysOnTop()
                   },
                 },
                 {
@@ -240,7 +245,7 @@ export default class MenuBuilder {
         submenu: [
           {
             label: "Visit on Github",
-            click() {
+            click: () => {
               shell.openExternal("https://github.com/infinitered/reactotron")
             },
           },
