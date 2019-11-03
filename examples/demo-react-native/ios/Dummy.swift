11430fda981cda5974ddc40734f1b5e798cff8e4
Richard Evans <rmevans9@gmail.com>
2019-11-02 22:14:21 -0500

    Add flipper to demo-react-native

---
+++ examples/demo-react-native/ios/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//  demoreactnative
+//
+//  Created by Richard Evans on 11/2/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
--- examples/demo-react-native/ios/demoreactnative/AppDelegate.m
+++ examples/demo-react-native/ios/demoreactnative/AppDelegate.m
@@ -11,10 +11,23 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#if DEBUG
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <ReactotronFlipper/Reactotron.h>
+#import <ReactotronFlipper/ReactotronFlipperPlugin.h>
+#endif
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [AppDelegate initializeFlipper:application];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"demoreactnative"
@@ -36,6 +49,20 @@
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
++ (void) initializeFlipper:(UIApplication *)application
+{
+#if DEBUG
+  FlipperClient *client = [FlipperClient sharedClient];
+  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+  [client addPlugin: [[FlipperKitLayoutPlugin alloc] initWithRootNode: application withDescriptorMapper: layoutDescriptorMapper]];
+  [client addPlugin: [[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+  [client addPlugin: [FlipperKitReactPlugin new]];
+  [client addPlugin: [[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+  [client addPlugin: [ReactotronFlipperPlugin new]];
+  [client start];
 #endif
 }
 
