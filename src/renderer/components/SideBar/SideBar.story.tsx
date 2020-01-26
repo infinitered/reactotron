451f1032df87e05da17878578640e147a8bb1653
Richard Evans <rmevans9@gmail.com>
2020-01-26 15:49:27 -0500

    fix a casing

--- src/renderer/components/SideBar/SideBar.story.tsx
+++ src/renderer/components/SideBar/SideBar.story.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 
-import SideBar from "./stateless"
+import SideBar from "./Stateless"
 
 export default {
   title: "SideBar",
