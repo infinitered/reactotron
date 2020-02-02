a02437e87d50a9205e02af0af8fe08b4e5c32667
Richard Evans <rmevans9@gmail.com>
2020-02-02 09:50:37 -0500

    fix: Prepping for notarizing

---
+++ scripts/notarize.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin' || !process.env.APPLEID) {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'com.yourcompany.yourAppId',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
