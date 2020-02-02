938a910f2524ae9b68f10a71e199e16af7d78250
Richard Evans <rmevans9@gmail.com>
2020-02-02 09:56:41 -0500

    fix: Actually setup notarize

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -10,7 +10,7 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    appBundleId: 'com.yourcompany.yourAppId',
+    appBundleId: 'com.reactotron.app',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
