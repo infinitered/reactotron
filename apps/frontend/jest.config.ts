1199bc8d01548f682c8c0d70d567fe02ca618c50
Richard Evans <rmevans9@gmail.com>
2022-05-23 13:52:33 -0400

    base frontend app

---
+++ apps/frontend/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/frontend',
+};
