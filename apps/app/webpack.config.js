c3319e0959961d63299b2f57191d18fee7a9247d
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-01-12 16:53:31 -0800

    fix(webpack): remove crypto.createHash('md5') in webpack to support Node 17+

---
+++ apps/app/webpack.config.js
@@ -0,0 +1,20 @@
+// webpack 4 has an issue on Node 17+ where openssl throws
+// an error when trying to call crypto.createHash('md5').
+// In order to fix this, we are providing a custom hash function.
+// See: https://stackoverflow.com/a/69476335
+
+// This config along with patch-package removes all the references to md5 in our node_modules webpack.
+// This solution is brittle because it's dependent on the webpack version, but will be fixed after upgrading to webpack 5.
+
+// If this breaks again, you search webpack in node_modules for `createHash\(('|")md4('|")\)`,
+// replace it with `createHash("sha256")`, then run patch-package again.
+
+module.exports = function(config) {
+  return {
+    ...config,
+    output: {
+      ...config.output,
+      hashFunction: "sha256",
+    },
+  }
+}
