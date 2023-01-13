8960405f1fe85b6f70409c085114368a901fcf2e
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-01-12 17:02:43 -0800

    refactor(package.json): update test packages

--- apps/app/src/renderer/contexts/Layout/useLayout.test.ts
+++ apps/app/src/renderer/contexts/Layout/useLayout.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks"
+import { act, renderHook } from "@testing-library/react-hooks/dom"
 
 import useLayout from "./useLayout"
 
--- apps/app/src/renderer/contexts/Standalone/useStandalone.test.ts
+++ apps/app/src/renderer/contexts/Standalone/useStandalone.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks"
+import { act, renderHook } from "@testing-library/react-hooks/dom"
 
 import useStandalone from "./useStandalone"
 
