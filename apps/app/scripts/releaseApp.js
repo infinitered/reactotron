ac35c5223f343f8ec9aa9df9a42acba32770aa83
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-01-23 11:52:32 -0800

    fix(.circleci): add cache_key, use CSC_LINK, larger resource_class

--- apps/app/scripts/releaseApp.js
+++ apps/app/scripts/releaseApp.js
@@ -5,13 +5,26 @@ if (BUILD_TARGET !== "unix" && BUILD_TARGET !== "windows") {
   throw new Error('BUILD_TARGET must be either "unix" or "windows"')
 }
 
+const path = require("path")
+const fs = require("fs")
+const CSC_LINK = path.join(__dirname, "../../../", "Certificates.p12")
+
+if (!fs.existsSync(CSC_LINK)) {
+  throw new Error(`CSC_LINK not found at ${CSC_LINK}`)
+}
+
 /** @see https://electron.build/cli.html */
 const targetFlags = { unix: "-ml -c.snap.publish=github", windows: "-w" }
 
 /** @param cmd {string} */
 const $ = cmd => {
   require("child_process").execSync(cmd, {
-    env: { ...process.env, BUILD_TARGET },
+    env: {
+      ...process.env,
+      BUILD_TARGET,
+      CSC_LINK,
+      CSC_IDENTITY_AUTO_DISCOVERY: "false",
+    },
     stdio: "inherit",
   })
 }
