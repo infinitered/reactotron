facbb89b7a670675c668e27ec5ab46d98de42363
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-01-25 09:57:42 -0800

    fix(release.artifacts.mjs): handle validation better

--- scripts/release.artifacts.mjs
+++ scripts/release.artifacts.mjs
@@ -2,18 +2,29 @@
 // @ts-check
 import "zx/globals";
 
+const isCi = process.env.CI === "true";
 const [_NODE_PATH, _ZX_PATH, _SCRIPT_PATH, ...args] = process.argv;
 const [gitTag] = args;
-const isCi = process.env.CI === "true";
+const [npmWorkspace, version] = gitTag.split("@");
 
 // #region assert tag matches 'app@1.1.1' format
-if (!gitTag || !gitTag.match(/^[a-z0-9-]+@[0-9]+\.[0-9]+\.[0-9]+$/)) {
+const GIT_TAG_REGEX = /^[a-z0-9-]+@[a-z0-9\.-]+$/; // 'reactotron-app@3.0.0' | 'reactotron-core-ui@2.0.1'
+/** @see https://gist.github.com/jhorsman/62eeea161a13b80e39f5249281e17c39 */
+const SEM_VER_REGEX = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/;
+if (
+  !gitTag ||
+  !gitTag.match(GIT_TAG_REGEX) ||
+  !npmWorkspace ||
+  !version ||
+  !version.match(SEM_VER_REGEX)
+) {
   console.error(
     `First argument must match format: <workspace>@<version>, got '${gitTag}'`
   );
   process.exit(1);
 }
 console.log(`Creating release for '${gitTag}'`);
+
 // #endregion
 
 // #region assert GITHUB_TOKEN is set
@@ -32,8 +43,6 @@ const npmTag = gitTag.includes("beta")
   : gitTag.includes("alpha")
   ? "alpha"
   : "latest";
-
-const [npmWorkspace, version] = gitTag.split("@");
 
 console.log(`Creating npm release for: ${gitTag}`);
 $`npm publish --access public --tag ${npmTag} --workspace ${npmWorkspace} --dry-run ${!isCi}`;
