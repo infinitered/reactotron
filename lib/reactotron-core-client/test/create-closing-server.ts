7655bdffb7493ac8acf908dd7bb339607ff20990
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-01-27 10:41:45 -0800

    fix(reactotron-core-client): close clients in createClosingServer

--- lib/reactotron-core-client/test/create-closing-server.ts
+++ lib/reactotron-core-client/test/create-closing-server.ts
@@ -2,14 +2,17 @@ import * as WebSocket from "ws"
 
 /**
  * A server which shuts down after somebody connects.
- * 
+ *
  * @param port The port to listen on
  * @param done Calls after we close.
  */
 export function createClosingServer(port: number, onDone?: () => void) {
   const wss = new WebSocket.Server({ port })
   wss.on("connection", () => {
-    wss.close()
     onDone && onDone()
+    wss.clients.forEach((client) => {
+      client.close()
+    })
+    wss.close()
   })
 }
