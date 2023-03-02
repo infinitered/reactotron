ad3b747f4d7992231cc1f646bfedbade62c145a2
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-03-01 17:14:30 -0800

    fix(reactotron-core-server): close server and clients in test

--- lib/reactotron-core-server/test/configure.test.ts
+++ lib/reactotron-core-server/test/configure.test.ts
@@ -3,11 +3,13 @@ import { createServer } from "../src/reactotron-core-server"
 test("has default port", () => {
   const client = createServer()
   expect(client.options.port).toBe(9090)
+  client.on("connect", () => client.stop())
 })
 
 test("options can be overridden", () => {
   const server = createServer({ port: 1 })
   expect(server.options.port).toBe(1)
+  server.on("connect", () => server.stop())
 })
 
 test("port must be valid", () => {
