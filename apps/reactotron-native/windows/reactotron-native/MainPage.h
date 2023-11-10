83169239c6f5d7e9e5d425d168f5b58551c5291e
Ryan Linton <ryanlntn@gmail.com>
2023-11-10 14:27:37 -0700

    add initial windows app

---
+++ apps/reactotron-native/windows/reactotron-native/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reactotron-native::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reactotron-native::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
