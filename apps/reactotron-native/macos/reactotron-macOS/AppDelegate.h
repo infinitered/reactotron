fcfcd15ba872650f08cb6990445e562c42ded109
Ryan Linton <ryanlntn@gmail.com>
2023-11-10 14:09:35 -0700

    spin up initial macos app

---
+++ apps/reactotron-native/macos/reactotron-macOS/AppDelegate.h
@@ -0,0 +1,6 @@
+#import <RCTAppDelegate.h>
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : RCTAppDelegate
+
+@end
---
+++ apps/reactotron-native/metro.config.js
@@ -0,0 +1,14 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  watchFolders: [path.resolve(__dirname, '../../node_modules')],
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
