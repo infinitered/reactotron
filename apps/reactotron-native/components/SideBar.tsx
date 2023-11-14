2e609bb78133d254ec6596754a698a3866e1bb85
Ryan Linton <ryanlntn@gmail.com>
2023-11-14 11:43:45 -0700

    tweak sidebar some more

--- apps/reactotron-native/components/SideBar.tsx
+++ apps/reactotron-native/components/SideBar.tsx
@@ -1,37 +1,28 @@
 import { useNavigation } from "@react-navigation/native"
 import { AlignJustify, Database, HelpCircle, Smartphone, Wand2 } from "@tamagui/lucide-icons"
-import { Button, Image, View, YStack } from "tamagui"
+import { Button, ButtonProps, Image, View, YStack } from "tamagui"
 import { reactotronLogo } from "../assets/images"
+
+const SideBarItem = ({ title, ...props }: ButtonProps & { title: string }) => (
+  <Button children={title} jc="flex-start" mb="$2" {...props} />
+)
 
 export function SideBar() {
   const navigation = useNavigation()
   const goTo = (name: keyof ReactNavigation.RootParamList) => () => navigation.navigate(name)
   return (
     <YStack bg="$gray2" p="$2">
-      <Button
+      <SideBarItem
+        title="Home"
         icon={() => <Image h={16} w={16} source={reactotronLogo} />}
-        jc="flex-start"
-        mb="$2"
         onPress={goTo("Home")}
-      >
-        Home
-      </Button>
-      <Button icon={AlignJustify} jc="flex-start" mb="$2" onPress={goTo("Timeline")}>
-        Timeline
-      </Button>
-      <Button icon={Database} jc="flex-start" mb="$2" onPress={goTo("Subscriptions")}>
-        State
-      </Button>
-      <Button icon={Smartphone} jc="flex-start" mb="$2" onPress={goTo("Overlay")}>
-        React Native
-      </Button>
-      <Button icon={Wand2} jc="flex-start" mb="$2" onPress={goTo("Commands")}>
-        Commands
-      </Button>
+      />
+      <SideBarItem title="Timeline" icon={AlignJustify} onPress={goTo("Timeline")} />
+      <SideBarItem title="State" icon={Database} onPress={goTo("Subscriptions")} />
+      <SideBarItem title="React Native" icon={Smartphone} onPress={goTo("Overlay")} />
+      <SideBarItem title="Commands" icon={Wand2} onPress={goTo("Commands")} />
       <View f={1} />
-      <Button icon={HelpCircle} jc="flex-start" mb="$2" onPress={goTo("Help")}>
-        Help
-      </Button>
+      <SideBarItem title="Help" icon={HelpCircle} onPress={goTo("Help")} />
     </YStack>
   )
 }
