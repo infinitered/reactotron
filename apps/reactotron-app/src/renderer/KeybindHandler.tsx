d79e4bccaad529bb16e07d9fcac06e0d957651e7
Joshua Yoes <joshua.r.yoes@gmail.com>
2023-12-15 12:21:59 -0800

    Add Preferences tab to Help shortcuts, cleanup Typescript types

--- apps/reactotron-app/src/renderer/KeybindHandler.tsx
+++ apps/reactotron-app/src/renderer/KeybindHandler.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react"
-import { GlobalHotKeys, KeyEventName } from "react-hotkeys"
+import { GlobalHotKeys, type KeyMap } from "react-hotkeys"
 import { ReactotronContext, StateContext } from "reactotron-core-ui"
 import LayoutContext from "./contexts/Layout"
 
@@ -9,51 +9,57 @@ const keyMap = {
     name: "Toggle Sidebar",
     group: "Application",
     sequences: ["command+shift+s", "ctrl+shift+s"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   // Tab Navigation
   OpenHomeTab: {
     name: "Home tab",
     group: "Navigation",
     sequences: ["command+1", "ctrl+1"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   OpenTimelineTab: {
     name: "Timeline tab",
     group: "Navigation",
     sequences: ["command+2", "ctrl+2"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   OpenStateTab: {
     name: "State tab",
     group: "Navigation",
     sequences: ["command+3", "ctrl+3"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   OpenReactNativeTab: {
     name: "React Native tab",
     group: "Navigation",
     sequences: ["command+4", "ctrl+4"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   OpenCustomCommandsTab: {
     name: "Custom Commands tab",
     group: "Navigation",
     sequences: ["command+5", "ctrl+5"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
+  },
+  OpenPreferencesTab: {
+    name: "Preferences tab",
+    group: "Navigation",
+    sequences: ["command+,", "ctrl+,"],
+    action: "keyup",
   },
   OpenHelpTab: {
     name: "Help tab",
     group: "Navigation",
     sequences: ["command+?", "ctrl+?"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   // Timeline
   ClearTimeline: {
     name: "Clear Timeline",
     group: "Timeline",
     sequences: ["command+k", "ctrl+k"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   // Modals
   // TODO: What keybinding should this be set to?
@@ -61,27 +67,29 @@ const keyMap = {
   //   name: "Find keys or values",
   //   group: "State",
   //   sequences: ["command+k", "ctrl+k"],
-  //   action: "keyup" as KeyEventName,
+  //   action: "keyup" ,
   // },
   OpenSubscriptionModal: {
     name: "Open Subscription modal",
     group: "State",
     sequences: ["command+n", "ctrl+n"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   OpenDispatchModal: {
     name: "Open Dispatch modal",
     group: "State",
     sequences: ["command+d", "ctrl+d"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
   TakeSnapshot: {
     name: "Take snapshot",
     group: "State",
     sequences: ["command+s", "ctrl+s"],
-    action: "keyup" as KeyEventName,
+    action: "keyup",
   },
-}
+} satisfies KeyMap
+
+type ActionName = keyof typeof keyMap
 
 function KeybindHandler({ children }) {
   const { toggleSideBar } = useContext(LayoutContext)
@@ -104,6 +112,9 @@ function KeybindHandler({ children }) {
     },
     OpenCustomCommandsTab: () => {
       window.location.hash = "/customCommands"
+    },
+    OpenPreferencesTab: () => {
+      window.location.hash = "/preferences"
     },
     OpenHelpTab: () => {
       window.location.hash = "/help"
@@ -130,10 +141,10 @@ function KeybindHandler({ children }) {
     ClearTimeline: () => {
       clearCommands()
     },
-  }
+  } satisfies Record<ActionName, (keyEvent: KeyboardEvent) => void>
 
   return (
-    <GlobalHotKeys keyMap={keyMap as any} handlers={handlers}>
+    <GlobalHotKeys keyMap={keyMap} handlers={handlers}>
       {children}
     </GlobalHotKeys>
   )
