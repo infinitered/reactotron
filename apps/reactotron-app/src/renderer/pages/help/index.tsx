7f8ff1de7669ae0f59b09d6c9cd760389c18875e
Robin Heinze <robin.m.heinze@gmail.com>
2024-01-05 09:47:11 -0800

    fix: rename Repo on settings screen

--- apps/reactotron-app/src/renderer/pages/help/index.tsx
+++ apps/reactotron-app/src/renderer/pages/help/index.tsx
@@ -99,7 +99,7 @@ function Help() {
             <ItemIconContainer>
               <RepoIcon size={40} />
             </ItemIconContainer>
-            Repo
+            GitHub Docs
           </ItemContainer>
           <ItemContainer onClick={openFeedback}>
             <ItemIconContainer>
