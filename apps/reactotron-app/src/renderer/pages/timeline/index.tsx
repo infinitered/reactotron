7e3711c2833f7400f7a79662a41f4814869b4dcc
Carlin Isaacson <carlin@infinite.red>
2024-01-05 10:25:55 -0800

    feat: add quick start button to timeline

--- apps/reactotron-app/src/renderer/pages/timeline/index.tsx
+++ apps/reactotron-app/src/renderer/pages/timeline/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react"
-import { clipboard } from "electron"
+import { clipboard, shell } from "electron"
 import fs from "fs"
 import {
   Header,
@@ -19,13 +19,11 @@ const Container = styled.div`
   flex-direction: column;
   width: 100%;
 `
-
 const TimelineContainer = styled.div`
   height: 100%;
   overflow-y: auto;
   overflow-x: hidden;
 `
-
 const SearchContainer = styled.div`
   display: flex;
   align-items: center;
@@ -46,6 +44,26 @@ const SearchInput = styled.input`
   border: none;
   color: ${(props) => props.theme.foregroundDark};
   font-size: 14px;
+`
+const HelpMessage = styled.div`
+  margin: 0 40px;
+`
+const ButtonContainer = styled.div`
+  display: flex;
+  padding: 4px 8px;
+  margin: 30px 20px;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.backgroundLighter};
+  color: ${(props) => props.theme.foreground};
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`
+const Divider = styled.div`
+  height: 1px;
+  background-color: ${(props) => props.theme.foregroundDark};
+  margin: 40px 10px;
 `
 
 function Timeline() {
@@ -72,6 +90,10 @@ function Timeline() {
 
   const dispatchAction = (action: any) => {
     sendCommand("state.action.dispatch", { action })
+  }
+
+  function openDocs() {
+    shell.openExternal("https://docs.infinite.red/reactotron/quick-start/react-native/")
   }
 
   return (
@@ -120,6 +142,13 @@ function Timeline() {
       <TimelineContainer>
         {filteredCommands.length === 0 ? (
           <EmptyState icon={MdReorder} title="No Activity">
+            <HelpMessage>
+              Once your app connects and starts sending events, they will appear here.
+            </HelpMessage>
+            <ButtonContainer onClick={openDocs}>
+              Check out the quick start guide here!
+            </ButtonContainer>
+            <Divider />
             <RandomJoke />
           </EmptyState>
         ) : (
