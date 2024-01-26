4c9e2308d6bc1651bec5bcd5bf31df97976832ba
Mark Rickert <mjar81@gmail.com>
2024-01-25 17:40:11 -0700

    Remove unused icons from the example-app

--- apps/example-app/app/components/Icon.tsx
+++ apps/example-app/app/components/Icon.tsx
@@ -85,19 +85,8 @@ export function Icon(props: IconProps) {
 }
 
 export const iconRegistry = {
-  back: require("../../assets/icons/back.png"),
-  bell: require("../../assets/icons/bell.png"),
-  caretLeft: require("../../assets/icons/caretLeft.png"),
   caretRight: require("../../assets/icons/caretRight.png"),
-  check: require("../../assets/icons/check.png"),
-  hidden: require("../../assets/icons/hidden.png"),
   ladybug: require("../../assets/icons/ladybug.png"),
-  lock: require("../../assets/icons/lock.png"),
-  menu: require("../../assets/icons/menu.png"),
-  more: require("../../assets/icons/more.png"),
-  settings: require("../../assets/icons/settings.png"),
-  view: require("../../assets/icons/view.png"),
-  x: require("../../assets/icons/x.png"),
 }
 
 const $imageStyleBase: ImageStyle = {
