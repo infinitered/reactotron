700c85c4458b896251ec6ea2fd6cd8fec7d4d63c
Ryan Linton <ryanlntn@gmail.com>
2024-06-20 19:30:14 -0600

    get RN upgraded macOS building

--- apps/reactotron-native/App.tsx
+++ apps/reactotron-native/App.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { Provider } from "./contexts"
-import { Navigation } from "./navigation"
+// import { Navigation } from "./navigation"
+import { HomeScreen } from "./screens/HomeScreen"
 
 export function App() {
   return (
     <Provider>
-      <Navigation />
+      <HomeScreen />
+
+      {/* <Navigation /> */}
     </Provider>
   )
 }
--- apps/reactotron-native/macos/reactotron-native-macOS/AppDelegate.mm
+++ apps/reactotron-native/macos/reactotron-native-macOS/AppDelegate.mm
@@ -16,15 +16,24 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
-}
-
-- (NSURL *)getBundleURL
-{
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+#ifdef RN_FABRIC_ENABLED
+  return true;
+#else
+  return false;
 #endif
 }
 
