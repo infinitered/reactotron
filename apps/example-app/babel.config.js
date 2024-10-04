1f05c7c03adf7e8294ca342d9f5fc53f3323d30a
Frank Calise <fcalise@gmail.com>
2024-10-04 17:33:42 -0400

    fix(example-app): remove react-native-reanimated (#1509 by @frankcalise)
    [skip ci]
    
    ## Describe your PR
    - Removes `react-native-reanimated` dependency from the Example App
    - Simplifies the Example App for easier contributions (in the terms of,
    it's less of a headache to build, see Why)
    - Tested building Android, switching between old and new arch without
    any hiccups or patching that @morganick has begun to push upstream
    
    ## Why
    - It's been trouble in a monorepo setting
    - We don't use it in the example app, it was just from Ignite spin up

--- apps/example-app/babel.config.js
+++ apps/example-app/babel.config.js
@@ -1,17 +1,12 @@
 /** @type {import('@babel/core').TransformOptions['plugins']} */
-const plugins = [
-  /** react-native-reanimated web support @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#web */
-  "@babel/plugin-proposal-export-namespace-from",
-  /** NOTE: This must be last in the plugins @see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/installation/#babel-plugin */
-  "react-native-reanimated/plugin",
-]
+const plugins = []
 
 /** @type {import('@babel/core').TransformOptions} */
 module.exports = function (api) {
   api.cache(true)
   return {
     assumptions: {
-      "setPublicClassFields": true,
+      setPublicClassFields: true,
     },
     presets: ["babel-preset-expo"],
     env: {
