4e1888631c2f342bc02af25709b8f42bc8a485e4
Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>
2024-12-20 13:00:12 -0800

    Moar comments

--- lib/reactotron-react-native/src/plugins/devTools.ts
+++ lib/reactotron-react-native/src/plugins/devTools.ts
@@ -2,35 +2,43 @@ import { Platform, TurboModuleRegistry } from "react-native"
 import type { ReactotronCore, Plugin } from "reactotron-core-client"
 import type { Spec } from "react-native/Libraries/NativeModules/specs/NativeDevMenu"
 
-const notAvailable = (method: string) => {
-  console.warn(`DevMenu.${method}() not available in this environment`)
-}
-
-const stubDevMenu: Spec = {
-  reload() {
-    notAvailable("reload")
-  },
-  show() {
-    notAvailable("show")
-  },
-  getConstants() {
-    return {}
-  },
-  debugRemotely() {
-    notAvailable("debugRemotely")
-  },
-  setHotLoadingEnabled() {
-    notAvailable("setHotLoadingEnabled")
-  },
-  setProfilingEnabled() {
-    notAvailable("setProfilingEnabled")
-  },
-}
-
+/**
+ * Lazily get the DevMenu module.
+ *
+ * This avoids trying a potentially risky call to React Native internals unless our dear developer actually wants to use it.
+ */
 const getDevMenu = (): Spec => {
+  const notAvailable = (method: string) => {
+    console.warn(`DevMenu.${method}() not available in this environment`)
+  }
+
+  const stubDevMenu: Spec = {
+    reload() {
+      notAvailable("reload")
+    },
+    show() {
+      notAvailable("show")
+    },
+    getConstants() {
+      return {}
+    },
+    debugRemotely() {
+      notAvailable("debugRemotely")
+    },
+    setHotLoadingEnabled() {
+      notAvailable("setHotLoadingEnabled")
+    },
+    setProfilingEnabled() {
+      notAvailable("setProfilingEnabled")
+    },
+  }
+
   if (Platform.OS === "ios" && __DEV__) {
     try {
+      // use TurboModuleRegistry.get instead of TurboModuleRegistry.getEnforcing, like at
+      // https://github.com/facebook/react-native/blob/main/packages/react-native/src/private/specs/modules/NativeDevMenu.js#L23
       const DevMenu = TurboModuleRegistry.get<Spec>("DevMenu")
+      // the DevMenu module is not available in all environments, like Expo Go, so we need to check if it exists
       if (DevMenu) return DevMenu
       return stubDevMenu
     } catch {
--- types/react-native/Libraries/NativeModules/specs/NativeDevMenu.d.ts
+++ types/react-native/Libraries/NativeModules/specs/NativeDevMenu.d.ts
@@ -1,6 +1,7 @@
 declare module "react-native/Libraries/NativeModules/specs/NativeDevMenu" {
   import type { TurboModule } from "react-native"
   /**
+   * @see https://github.com/facebook/react-native/blob/main/packages/react-native/Libraries/NativeModules/specs/NativeDevMenu.js
    * @see https://github.com/facebook/react-native/blob/main/packages/react-native/src/private/specs/modules/NativeDevMenu.js#L15-L21
    */
   export interface Spec extends TurboModule {
