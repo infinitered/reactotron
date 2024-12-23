5f692ac953e73d131eaff6e403afc2211ace9ca2
Frank Calise <fcalise@gmail.com>
2024-12-22 21:43:51 -0500

    fix(reactotron-react-native): only set DevMenu when in __DEV__ (#1527 by @frankcalise and @joshuayoes)
    ## Please verify the following:
    
    - [x] `yarn build-and-test:local` passes
    - [ ] I have added tests for any new features, if relevant
    - [ ] `README.md` (or relevant documentation) has been updated with your
    changes
    
    ## Describe your PR
    - Closes #1513 
    - Before we improperly included the DevMenu module in both debug and
    release builds, this checks the `__DEV__` flag
    - Now we access the module using `TurboModuleRegistry.get` instead of
    `TurboModuleRegistry.getEnforcing` to avoid throwing a top level JS
    error when it does not exist
    
    ---------
    
    Co-authored-by: Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>

--- lib/reactotron-react-native/src/plugins/devTools.ts
+++ lib/reactotron-react-native/src/plugins/devTools.ts
@@ -1,9 +1,52 @@
-import { Platform } from "react-native"
+import { Platform, TurboModuleRegistry } from "react-native"
 import type { ReactotronCore, Plugin } from "reactotron-core-client"
+import type { Spec } from "react-native/Libraries/NativeModules/specs/NativeDevMenu"
 
-let DevMenu = { show: () => {}, reload: () => {} }
-if (Platform.OS === "ios") {
-  DevMenu = require("react-native/Libraries/NativeModules/specs/NativeDevMenu")
+/**
+ * Lazily get the DevMenu module.
+ *
+ * This avoids trying a potentially risky call to React Native internals unless our dear developer actually wants to use it.
+ */
+const getDevMenu = (): Spec => {
+  const notAvailable = (method: string) => {
+    console.warn(`DevMenu.${method}() not available in this environment`)
+  }
+
+  const stubDevMenu: Spec = {
+    reload() {
+      notAvailable("reload")
+    },
+    show() {
+      notAvailable("show")
+    },
+    getConstants() {
+      return {}
+    },
+    debugRemotely() {
+      notAvailable("debugRemotely")
+    },
+    setHotLoadingEnabled() {
+      notAvailable("setHotLoadingEnabled")
+    },
+    setProfilingEnabled() {
+      notAvailable("setProfilingEnabled")
+    },
+  }
+
+  if (Platform.OS === "ios" && __DEV__) {
+    try {
+      // use TurboModuleRegistry.get instead of TurboModuleRegistry.getEnforcing, like at
+      // https://github.com/facebook/react-native/blob/main/packages/react-native/src/private/specs/modules/NativeDevMenu.js#L23
+      const DevMenu = TurboModuleRegistry.get<Spec>("DevMenu")
+      // the DevMenu module is not available in all environments, like Expo Go, so we need to check if it exists
+      if (DevMenu) return DevMenu
+      return stubDevMenu
+    } catch {
+      return stubDevMenu
+    }
+  }
+
+  return stubDevMenu
 }
 
 const devTools = () => () => {
@@ -12,10 +55,12 @@ const devTools = () => () => {
       if (command.type !== "devtools.open" && command.type !== "devtools.reload") return
 
       if (command.type === "devtools.open") {
+        const DevMenu = getDevMenu()
         DevMenu.show()
       }
 
       if (command.type === "devtools.reload") {
+        const DevMenu = getDevMenu()
         DevMenu.reload()
       }
     },
