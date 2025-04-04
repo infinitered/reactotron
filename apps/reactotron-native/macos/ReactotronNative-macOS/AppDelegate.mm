959a90a2aff50b7a89c98d05775684117bd40dae
Jamon Holmgren <code@jamon.dev>
2025-04-04 15:15:35 -0700

    WIP everything broken

--- apps/reactotron-native/macos/ReactotronNative-macOS/AppDelegate.mm
+++ apps/reactotron-native/macos/ReactotronNative-macOS/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-  self.moduleName = @"ReactotronSeventyeight";
+  self.moduleName = @"ReactotronNative";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
