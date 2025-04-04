d06b3d4526faeb8030c9c8c4a1096168e7ad1158
Jamon Holmgren <code@jamon.dev>
2025-04-04 15:54:49 -0700

    WIP everything broken

--- apps/reactotron-native/metro.config.js
+++ apps/reactotron-native/metro.config.js
@@ -1,42 +1,11 @@
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
+
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
  *
- * @format
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
-const fs = require('fs');
-const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const config = {}
 
-const rnwPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
-);
-
-module.exports = {
-  resolver: {
-    blockList: exclusionList([
-      // This stops "react-native run-windows" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-      ),
-      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
-      new RegExp(`${rnwPath}/build/.*`),
-      new RegExp(`${rnwPath}/target/.*`),
-      /.*\.ProjectImports\.zip/,
-    ]),
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-    // This fixes the 'missing-asset-registry-path` error (see https://github.com/microsoft/react-native-windows/issues/11437)
-    assetRegistryPath: 'react-native/Libraries/Image/AssetRegistry',
-  },
-  watchFolders: [
-    path.resolve(__dirname, '../../lib'),
-    path.resolve(__dirname, '../../node_modules'),
-  ],
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
