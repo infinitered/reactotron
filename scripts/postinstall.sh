e73d782f4ff97ef51b3535639c83b718a1114fa8
Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>
2025-10-01 09:58:17 -0700

    chore(circleci): update Docker image and Node version (#1574)
    Add optional build jobs for manual approval
    
    ## Please verify the following:
    
    - [x] `yarn build-and-test:local` passes
    - [ ] I have added tests for any new features, if relevant
    - [ ] `README.md` (or relevant documentation) has been updated with your
    changes
    
    ## Describe your PR
    
    This PR attempts to upgrade the Node version in the CI jobs that are
    failing in
    https://app.circleci.com/pipelines/github/infinitered/reactotron/2658
    
    ---------
    
    Co-authored-by: Sean Barker <theseanbarker@gmail.com>
    Co-authored-by: Sean Barker <43788519+SeanBarker182@users.noreply.github.com>

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ "$SKIP_LIB_BUILD" = "1" ]; then
+  echo "SKIP_LIB_BUILD=1, skipping workspace build"
+  exit 0
+fi
+
 echo "Build all workspace packages to ensure all internal dependencies are up to date"
 yarn build
